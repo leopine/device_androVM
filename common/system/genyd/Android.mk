757257c4c13802a42d2924b5cc4b163915f71ffa
Julien Bolard <jbolard@genymobile.com>
2013-03-19 14:37:02 +0100

    [US41] Add new genyd daemon project structure

---
+++ common/system/genyd/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=                                      \
+                  main.cpp                             \
+
+
+LOCAL_MODULE:= genyd
+
+LOCAL_C_INCLUDES := $(KERNEL_HEADERS) \
+                    bionic \
+
+LOCAL_CFLAGS := -Werror=format
+
+LOCAL_SHARED_LIBRARIES := libsysutils libcutils libnetutils liblog \
+
+include $(BUILD_EXECUTABLE)
