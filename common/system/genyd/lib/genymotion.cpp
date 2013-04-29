faab8d658b89ece6edb4116505a05f685a38f904
Samuel Rats <srats@genymobile.com>
2013-04-29 15:56:55 +0200

    [US20] Base for libgenyd

--- common/system/genyd/lib/genymotion.cpp
+++ common/system/genyd/lib/genymotion.cpp
@@ -1,7 +1,75 @@
-#include "lib/genymotion.hpp"
+
+#include "genymotion.hpp"
+
+static Genymotion __instance;
+
+Genymotion &Genymotion::getInstance(void)
+{
+    return __instance;
+}
+
+// cTor
+Genymotion::Genymotion(void)
+{
+    callbacks["/sys/class/power_supply"] = &Genymotion::batteryCallback;
+
+    battery_callbacks["/health"] = &Genymotion::batteryStatus;
+}
+
+// dTor
+Genymotion::~Genymotion(void)
+{
+
+}
 
 // Overload /proc values with genymotion configuration
-int Genymotion::getValueFromProc(const char* path, char *buf, size_t size)
+int Genymotion::getValueFromProc(const char *path, char *buf, size_t size)
+{
+    Genymotion &instance = Genymotion::getInstance();
+
+    Genymotion::t_dispatcher_member callback = instance.getCallback(path);
+
+    if (callback)
+	return (instance.*callback)(path, buf, size);
+    return -1;
+}
+
+Genymotion::t_dispatcher_member Genymotion::getCallback(const char *path)
+{
+    std::map<std::string, Genymotion::t_dispatcher_member>::iterator begin = callbacks.begin();
+    std::map<std::string, Genymotion::t_dispatcher_member>::iterator end = callbacks.end();
+
+    std::string haystack(path);
+
+    while (begin != end) {
+	// if haystack starts with
+	if (haystack.find(begin->first) == 0)
+	    return begin->second;
+	++begin;
+    }
+
+    return NULL;
+}
+
+int Genymotion::batteryCallback(const char *path, char *buff, size_t size)
+{
+    std::map<std::string, Genymotion::t_callback_member>::iterator begin = battery_callbacks.begin();
+    std::map<std::string, Genymotion::t_callback_member>::iterator end = battery_callbacks.end();
+
+    std::string haystack(path);
+
+    while (begin != end) {
+	size_t pos = haystack.rfind(begin->first);
+	// if haystack ends with
+	if (pos != std::string::npos && pos + begin->first.size() == haystack.size())
+	    return (this->*(begin->second))(buff, size);
+	++begin;
+    }
+
+    return -1;
+}
+
+int Genymotion::batteryStatus(char *buff, size_t size)
 {
     return -1;
 }
--- common/system/genyd/lib/genymotion.hpp
+++ common/system/genyd/lib/genymotion.hpp
@@ -1,6 +1,9 @@
 #ifndef LIB_GENYMOTION_HPP_
 #define LIB_GENYMOTION_HPP_
 
+#include <map>
+#include <string>
+
 class Genymotion {
 
 public:
@@ -11,9 +14,28 @@ private:
     Genymotion(const Genymotion &);
     Genymotion operator=(const Genymotion &);
 
+    static Genymotion &getInstance(void);
+
+    // Callbacks lists
+    typedef int (Genymotion::*t_dispatcher_member)(const char *, char *, size_t);
+    std::map<std::string, t_dispatcher_member> callbacks;
+
+    typedef int (Genymotion::*t_callback_member)(char *, size_t);
+    std::map<std::string, t_callback_member> battery_callbacks;
+
 public:
     // Overload /proc values with genymotion configuration
     static int getValueFromProc(const char* path, char *buf, size_t size);
+
+private:
+    // Global dispatcher
+    t_dispatcher_member getCallback(const char *path);
+
+    // Battery callbacks
+    // Battery dispatcher
+    int batteryCallback(const char *path, char *buff, size_t size);
+    // Battery status
+    int batteryStatus(char *buff, size_t size);
 };
 
 #endif // #define LIB_GENYMOTION_HPP_GENYD_READ_PROC_HPP_
