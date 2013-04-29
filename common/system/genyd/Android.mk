9030bc1f2fa8ad5652b8d49eb717339a3b69d95d
Morgan Germain <mgermain@genymobile.com>
2013-04-29 16:12:50 +0200

    [US20] links genyd to libgenyd

--- common/system/genyd/Android.mk
+++ common/system/genyd/Android.mk
@@ -1,35 +1,4 @@
-##################
-## Genyd daemon ##
-##################
-
-# Build executable
 LOCAL_PATH		:= $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES		:= main.cpp				\
-			   genyd.cpp				\
-			   socket.cpp           		\
-			   dispatcher.cpp			\
-		           $(call all-proto-files-under, .)
-
-LOCAL_MODULE		:= genyd
-
-LOCAL_C_INCLUDES	:= $(KERNEL_HEADERS)       		\
-			   bionic                  		\
-			   external/stlport/stlport		\
-			   external/protobuf/src
-
-LOCAL_MODULE_TAGS	:= optional
-
-LOCAL_CFLAGS		:= -Werror=format
-
-LOCAL_SHARED_LIBRARIES	:= libcutils libnetutils liblog libstlport
-
-LOCAL_PROTOC_OPTIMIZE_TYPE := full
-
-include $(BUILD_EXECUTABLE)
-
 
 ###################
 ## Genyd library ##
@@ -52,3 +21,34 @@ LOCAL_SHARED_LIBRARIES  := liblog libcutils libstlport
 LOCAL_MODULE            := libgenyd
 
 include $(BUILD_SHARED_LIBRARY)
+
+
+##################
+## Genyd daemon ##
+##################
+
+# Build executable
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES		:= main.cpp				\
+			   genyd.cpp				\
+			   socket.cpp           		\
+			   dispatcher.cpp			\
+		           $(call all-proto-files-under, .)
+
+LOCAL_MODULE		:= genyd
+
+LOCAL_C_INCLUDES	:= $(KERNEL_HEADERS)       		\
+			   bionic                  		\
+			   external/stlport/stlport		\
+			   external/protobuf/src
+
+LOCAL_MODULE_TAGS	:= optional
+
+LOCAL_CFLAGS		:= -Werror=format
+
+LOCAL_SHARED_LIBRARIES	:= libcutils libnetutils liblog libstlport libgenyd
+
+LOCAL_PROTOC_OPTIMIZE_TYPE := full
+
+include $(BUILD_EXECUTABLE)
