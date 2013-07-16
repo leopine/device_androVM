de8620518c2c92289ea051df5b64fbf030845aac
Samuel Rats <srats@genymobile.com>
2013-07-16 10:07:18 +0200

    Re-enable mouse events on virtual keyboard input mode

--- vbox86/vinput/vinput.c
+++ vbox86/vinput/vinput.c
@@ -19,66 +19,74 @@ int main(int argc, char *argv[]) {
     int csocket;
     int i;
 
+    int keyboard_disabled = 0;
+    char keyboard_prop[PROPERTY_VALUE_MAX];
+    property_get("androVM.keyboard_disable", keyboard_prop, "0");
+
+    if (!strcmp(keyboard_prop, "1")) {
+        keyboard_disabled = 1;
+    }
+
     while (1) {
-	FILE *f_sock;
+        FILE *f_sock;
 #define BUFSIZE 256
-	char mbuf[BUFSIZE];
+        char mbuf[BUFSIZE];
 
         int ssocket;
         ssocket = socket_inaddr_any_server(22469, SOCK_STREAM);
         if (ssocket < 0) {
-	    fprintf(stderr, "Unable to start listening server...\n");
-	    break;
-	}
+            fprintf(stderr, "Unable to start listening server...\n");
+            break;
+        }
         csocket = accept(ssocket, NULL, NULL);
         if (csocket < 0) {
-	    fprintf(stderr, "Unable to accept connection...\n");
+            fprintf(stderr, "Unable to accept connection...\n");
             close(ssocket);
-	    break;
-	}
+            break;
+        }
         close(ssocket);
 
 
-	f_sock = fdopen(csocket, "r");
-	if (!f_sock) {
-	    fprintf(stderr, "Unable to have fdsock on socket...\n");
-	    close(csocket);
-	    break;
-	}
+        f_sock = fdopen(csocket, "r");
+        if (!f_sock) {
+            fprintf(stderr, "Unable to have fdsock on socket...\n");
+            close(csocket);
+            break;
+        }
 
-	while (fgets(mbuf, BUFSIZE, f_sock)) {
-	    char *pcmd, *p1, *p2, *p3, *p4, *pb, *pe;
+        while (fgets(mbuf, BUFSIZE, f_sock)) {
+            char *pcmd, *p1, *p2, *p3, *p4, *pb, *pe;
 
-	    pcmd = p1 = p2 = NULL;
-	    if (pe=strchr(mbuf,'\n'))
-		*pe='\0';
-	    pcmd=pb=mbuf;
-	    if (pb && (pe=strchr(pb,':'))) {
-		*pe='\0';
-		p1=pb=++pe;
-	    }
-	    if (pb && (pe=strchr(pb,':'))) {
-		*pe='\0';
-		p2=pb=++pe;
-	    }
-	    if (pb && (pe=strchr(pb,':'))) {
-		*pe='\0';
-		p3=pb=++pe;
-	    }
-	    if (pb && (pe=strchr(pb,':'))) {
-		*pe='\0';
-		p4=pb=++pe;
-	    }
-	    if (!pcmd) 
-		continue;
+            pcmd = p1 = p2 = NULL;
+            if (pe=strchr(mbuf,'\n'))
+                *pe='\0';
+            pcmd=pb=mbuf;
+            if (pb && (pe=strchr(pb,':'))) {
+                *pe='\0';
+                p1=pb=++pe;
+            }
+            if (pb && (pe=strchr(pb,':'))) {
+                *pe='\0';
+                p2=pb=++pe;
+            }
+            if (pb && (pe=strchr(pb,':'))) {
+                *pe='\0';
+                p3=pb=++pe;
+            }
+            if (pb && (pe=strchr(pb,':'))) {
+                *pe='\0';
+                p4=pb=++pe;
+            }
+            if (!pcmd)
+                continue;
             if (!strcmp(pcmd,"CONFIG")) {
                 if (!p1 || !p2)
                     continue;
 
                 // Create ABS input device
                 if (!(uinp_fd = open("/dev/uinput", O_WRONLY|O_NDELAY))) {
-	          fprintf(stderr, "Unable to open /dev/uinput !\n");
-	          exit(-1);
+                    fprintf(stderr, "Unable to open /dev/uinput !\n");
+                    exit(-1);
                 }
 
                 memset(&uinp, 0, sizeof(uinp));
@@ -95,9 +103,10 @@ int main(int argc, char *argv[]) {
                 ioctl(uinp_fd, UI_SET_ABSBIT, ABS_X);
                 ioctl(uinp_fd, UI_SET_ABSBIT, ABS_Y);
                 ioctl(uinp_fd, UI_SET_ABSBIT, ABS_PRESSURE);
-                for (i=0;i<256;i++)
-                    ioctl(uinp_fd, UI_SET_KEYBIT, i);
-                //ioctl(uinp_fd, UI_SET_KEYBIT, BTN_MOUSE);
+                if (!keyboard_disabled) {
+                    for (i=0;i<256;i++)
+                        ioctl(uinp_fd, UI_SET_KEYBIT, i);
+                }
                 ioctl(uinp_fd, UI_SET_KEYBIT, BTN_TOUCH);
                 ioctl(uinp_fd, UI_SET_KEYBIT, BTN_LEFT);
                 ioctl(uinp_fd, UI_SET_KEYBIT, BTN_TOOL_PEN);
@@ -106,122 +115,122 @@ int main(int argc, char *argv[]) {
                 ioctl(uinp_fd, UI_SET_RELBIT, REL_WHEEL);
                 write(uinp_fd, &uinp, sizeof(uinp));
                 if (ioctl(uinp_fd, UI_DEV_CREATE)) {
-	            fprintf(stderr, "Unable to create ABS uinput device...\n");
-	            exit(-1);
+                    fprintf(stderr, "Unable to create ABS uinput device...\n");
+                    exit(-1);
                 }
             }
-	    else if (!strcmp(pcmd,"MOUSE")) {
-		if (!p1 || !p2)
-		    continue;
-		memset(&event, 0, sizeof(event));
-		gettimeofday(&event.time, NULL);
-		event.type = EV_ABS;
-		event.code = ABS_X;
-		event.value = atoi(p1);
-		write(uinp_fd, &event, sizeof(event));
-		event.type = EV_ABS;
-		event.code = ABS_Y;
-		event.value = atoi(p2);
-		write(uinp_fd, &event, sizeof(event));
-		event.type = EV_SYN;
-		event.code = SYN_REPORT;
-		event.value = 0;
-		write(uinp_fd, &event, sizeof(event));
-	    }
-	    else if (!strcmp(pcmd,"WHEEL")) {
-		if (!p1 || !p2 || !p4 || !p4) 
-		    continue;
-		memset(&event, 0, sizeof(event));
-		gettimeofday(&event.time, NULL);
-		event.type = EV_ABS;
-		event.code = ABS_X;
-		event.value = atoi(p1);
-		write(uinp_fd, &event, sizeof(event));
-		event.type = EV_ABS;
-		event.code = ABS_Y;
-		event.value = atoi(p2);
-		write(uinp_fd, &event, sizeof(event));
-		event.type = EV_REL;
-		event.code = REL_WHEEL;
-		event.value = atoi(p3);
-		write(uinp_fd, &event, sizeof(event));
-		event.type = EV_REL;
-		event.code = REL_HWHEEL;
-		event.value = atoi(p4);
-		write(uinp_fd, &event, sizeof(event));
-		event.type = EV_SYN;
-		event.code = SYN_REPORT;
-		event.value = 0;
-		write(uinp_fd, &event, sizeof(event));
-	    }
-	    else if (!strcmp(pcmd,"MSBPR")) {
-		if (!p1 || !p2) 
-		    continue;
-		memset(&event, 0, sizeof(event));
-		gettimeofday(&event.time, NULL);
-		event.type = EV_KEY;
-		event.code = BTN_TOUCH;
-		event.value = 1;
-		write(uinp_fd, &event, sizeof(event));
-		event.type = EV_ABS;
-		event.code = ABS_PRESSURE;
-		event.value = 1;
-		write(uinp_fd, &event, sizeof(event));
-		event.type = EV_SYN;
-		event.code = SYN_REPORT;
-		event.value = 0;
-		write(uinp_fd, &event, sizeof(event));
-	    }
-	    else if (!strcmp(pcmd,"MSBRL")) {
-		if (!p1 || !p2) 
-		    continue;
-		memset(&event, 0, sizeof(event));
-		gettimeofday(&event.time, NULL);
-		event.type = EV_KEY;
-		event.code = BTN_TOUCH;
-		event.value = 0;
-		write(uinp_fd, &event, sizeof(event));
-		event.type = EV_ABS;
-		event.code = ABS_PRESSURE;
-		event.value = 0;
-		write(uinp_fd, &event, sizeof(event));
-		event.type = EV_SYN;
-		event.code = SYN_REPORT;
-		event.value = 0;
-		write(uinp_fd, &event, sizeof(event));
-	    }
-	    else if (!strcmp(pcmd,"KBDPR")) {
-		if (!p1 || !p2) 
-		    continue;
-		memset(&event, 0, sizeof(event));
-		gettimeofday(&event.time, NULL);
-		event.type = EV_KEY;
-		event.code = atoi(p1);
-		event.value = 1;
-		write(uinp_fd, &event, sizeof(event));
-		event.type = EV_SYN;
-		event.code = SYN_REPORT;
-		event.value = 0;
-		write(uinp_fd, &event, sizeof(event));
-	    }
-	    else if (!strcmp(pcmd,"KBDRL")) {
-		if (!p1 || !p2) 
-		    continue;
-		memset(&event, 0, sizeof(event));
-		gettimeofday(&event.time, NULL);
-		event.type = EV_KEY;
-		event.code = atoi(p1);
-		event.value = 0;
-		write(uinp_fd, &event, sizeof(event));
-		event.type = EV_SYN;
-		event.code = SYN_REPORT;
-		event.value = 0;
-		write(uinp_fd, &event, sizeof(event));
-	    }
-	}
+            else if (!strcmp(pcmd,"MOUSE")) {
+                if (!p1 || !p2)
+                    continue;
+                memset(&event, 0, sizeof(event));
+                gettimeofday(&event.time, NULL);
+                event.type = EV_ABS;
+                event.code = ABS_X;
+                event.value = atoi(p1);
+                write(uinp_fd, &event, sizeof(event));
+                event.type = EV_ABS;
+                event.code = ABS_Y;
+                event.value = atoi(p2);
+                write(uinp_fd, &event, sizeof(event));
+                event.type = EV_SYN;
+                event.code = SYN_REPORT;
+                event.value = 0;
+                write(uinp_fd, &event, sizeof(event));
+            }
+            else if (!strcmp(pcmd,"WHEEL")) {
+                if (!p1 || !p2 || !p4 || !p4)
+                    continue;
+                memset(&event, 0, sizeof(event));
+                gettimeofday(&event.time, NULL);
+                event.type = EV_ABS;
+                event.code = ABS_X;
+                event.value = atoi(p1);
+                write(uinp_fd, &event, sizeof(event));
+                event.type = EV_ABS;
+                event.code = ABS_Y;
+                event.value = atoi(p2);
+                write(uinp_fd, &event, sizeof(event));
+                event.type = EV_REL;
+                event.code = REL_WHEEL;
+                event.value = atoi(p3);
+                write(uinp_fd, &event, sizeof(event));
+                event.type = EV_REL;
+                event.code = REL_HWHEEL;
+                event.value = atoi(p4);
+                write(uinp_fd, &event, sizeof(event));
+                event.type = EV_SYN;
+                event.code = SYN_REPORT;
+                event.value = 0;
+                write(uinp_fd, &event, sizeof(event));
+            }
+            else if (!strcmp(pcmd,"MSBPR")) {
+                if (!p1 || !p2)
+                    continue;
+                memset(&event, 0, sizeof(event));
+                gettimeofday(&event.time, NULL);
+                event.type = EV_KEY;
+                event.code = BTN_TOUCH;
+                event.value = 1;
+                write(uinp_fd, &event, sizeof(event));
+                event.type = EV_ABS;
+                event.code = ABS_PRESSURE;
+                event.value = 1;
+                write(uinp_fd, &event, sizeof(event));
+                event.type = EV_SYN;
+                event.code = SYN_REPORT;
+                event.value = 0;
+                write(uinp_fd, &event, sizeof(event));
+            }
+            else if (!strcmp(pcmd,"MSBRL")) {
+                if (!p1 || !p2)
+                    continue;
+                memset(&event, 0, sizeof(event));
+                gettimeofday(&event.time, NULL);
+                event.type = EV_KEY;
+                event.code = BTN_TOUCH;
+                event.value = 0;
+                write(uinp_fd, &event, sizeof(event));
+                event.type = EV_ABS;
+                event.code = ABS_PRESSURE;
+                event.value = 0;
+                write(uinp_fd, &event, sizeof(event));
+                event.type = EV_SYN;
+                event.code = SYN_REPORT;
+                event.value = 0;
+                write(uinp_fd, &event, sizeof(event));
+            }
+            else if (!strcmp(pcmd,"KBDPR")) {
+                if (!p1 || !p2)
+                    continue;
+                memset(&event, 0, sizeof(event));
+                gettimeofday(&event.time, NULL);
+                event.type = EV_KEY;
+                event.code = atoi(p1);
+                event.value = 1;
+                write(uinp_fd, &event, sizeof(event));
+                event.type = EV_SYN;
+                event.code = SYN_REPORT;
+                event.value = 0;
+                write(uinp_fd, &event, sizeof(event));
+            }
+            else if (!strcmp(pcmd,"KBDRL")) {
+                if (!p1 || !p2)
+                    continue;
+                memset(&event, 0, sizeof(event));
+                gettimeofday(&event.time, NULL);
+                event.type = EV_KEY;
+                event.code = atoi(p1);
+                event.value = 0;
+                write(uinp_fd, &event, sizeof(event));
+                event.type = EV_SYN;
+                event.code = SYN_REPORT;
+                event.value = 0;
+                write(uinp_fd, &event, sizeof(event));
+            }
+        }
 
-	close(csocket);
+        close(csocket);
     }
 
-    return 0;    
+    return 0;
 }
